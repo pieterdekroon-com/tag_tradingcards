ec543bb1a90cffa945c5e6eb8d0b2bb0a06e04a9
Pieter de Kroon <pieter@pieterdekroon.com>
2026-03-13 16:53:36 +0100

    feat: wire up App with state and side-by-side layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import { CardData } from './types';
+import { CardForm } from './components/CardForm';
+import { CardPreview } from './components/CardPreview';
+import styles from './App.module.css';
+
+const initialCard: CardData = {
+  id: crypto.randomUUID(),
+  name: '',
+  image: '',
+  specialties: [],
+  level: 0,
+  description: '',
+  background: 'ocean-blue',
+};
+
 function App() {
-  return <div>App</div>
+  const [card, setCard] = useState<CardData>(initialCard);
+
+  return (
+    <div className={styles.layout}>
+      <div className={styles.formPanel}>
+        <CardForm card={card} onChange={setCard} />
+      </div>
+      <div className={styles.previewPanel}>
+        <CardPreview card={card} />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
